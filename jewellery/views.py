44d6acea4bb15382b611a9420ab6f834653c7644
adamkcook94 <adamkcook94@hotmail.com>
2021-06-17 16:49:09 +0000

    added search functionality

--- jewellery/views.py
+++ jewellery/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Item
 
 # Create your views here.
@@ -7,9 +9,21 @@ from .models import Item
 def all_items(request):
 
     items = Item.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('items'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            items = items.filter(queries)
 
     context = {
         'items': items,
+        'search_term': query,
     }
 
     return render(request, 'items/items.html', context)
