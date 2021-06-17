e7e2276a5b638dca8c8da36ae2e1bdca871fe7ab
adamkcook94 <adamkcook94@hotmail.com>
2021-06-17 20:03:01 +0000

    added sorting and item counts

--- jewellery/views.py
+++ jewellery/views.py
@@ -11,8 +11,25 @@ def all_items(request):
     items = Item.objects.all()
     query = None
     item_cats = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                items = items.annotate(lower_name=Lower('name'))
+            if sortkey == 'item_cat':
+                sortkey = 'item_cat__name'
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            items = items.order_by(sortkey)
+
         if 'item_cat' in request.GET:
             item_cats = request.GET['item_cat'].split(',')
             items = items.filter(Item_Cat__name__in=item_cats)
@@ -27,10 +44,13 @@ def all_items(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             items = items.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'items': items,
         'search_term': query,
         'current_cat': item_cats,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'items/items.html', context)
