57194f20db7d28291a31471ce36b4ecf95bf7d17
adamkcook94 <adamkcook94@hotmail.com>
2021-06-17 15:01:43 +0000

    added items details functionality

--- jewellery/urls.py
+++ jewellery/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_items, name='item')
+    path('', views.all_items, name='all_items'),
+    path('<item_id>', views.items_description, name='items_description'),
 ]
