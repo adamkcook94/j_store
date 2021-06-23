ec82ee5543b127e2079abfa822edecde55969379
adamkcook94 <adamkcook94@hotmail.com>
2021-06-23 18:53:59 +0000

    Finished fixing image field in add/edit items

--- jewellery/forms.py
+++ jewellery/forms.py
@@ -14,7 +14,7 @@ class ItemForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         item_cats = Item_Cat.objects.all()
-        friendly_names = [(ic.id, ic.get_friendly_name()) for ic in item_cats]
+        friendly_names = [(c.id, c.get_friendly_name()) for c in item_cats]
 
         self.fields['Item_Cat'].choices = friendly_names
         for field_name, field in self.fields.items():
