a41329948ad4b52d4cece4a31f5fa96615b49c67
adamkcook94 <adamkcook94@hotmail.com>
2021-06-23 16:43:20 +0000

    added edit item functionality

--- jewellery/urls.py
+++ jewellery/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.all_items, name='all_items'),
     path('<int:item_id>/', views.items_description, name='items_description'),
     path('add/', views.add_item, name='add_item'),
+    path('edit/<int:item_id>/', views.edit_item, name='edit_item'),
 ]
