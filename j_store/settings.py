b41856458ff8ac22fc663103ed7acc5f8a1dc8b5
adamkcook94 <adamkcook94@hotmail.com>
2021-06-29 19:39:00 +0000

    removed secret key and changed debug option

--- j_store/settings.py
+++ j_store/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=@xp%c^1&j#d_1^n0$tvc*=1&=*70jb#6x-(lx*#w80zb8dja6'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['lusso-jewels.herokuapp.com', 'localhost']
 
