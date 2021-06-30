0f1519d7f30ca322eb6b40364ea8743ea4e01e92
adamkcook94 <adamkcook94@hotmail.com>
2021-06-30 01:29:17 +0000

    changed stripewhsecret key

--- j_store/settings.py
+++ j_store/settings.py
@@ -211,6 +211,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
-STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET',
-                             'whsec_gf1rvhiERiQZK0dAImDYETliZG8E54r1')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_FROM_EMAIL = 'jstore.example.com'
+
+# latest key: whsec_fpNBZArOVfCFPzIKvwbmzWaGeCRj7QfA
